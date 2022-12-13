bbd6f99c52ea443617cc7bfc460904faed576f05
liaoliao-yd <bgf@bgfyd.com>
2022-12-13 13:07:04 +0800

    优化变量

--- imgToTxt.py
+++ imgToTxt.py
@@ -5,10 +5,9 @@ from paddleocr import PaddleOCR
 ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
 
 
-def img_to_txt(dd, alt=None):
+def img_to_txt(img_path, alt=None):
     if alt is None:
         alt = []
-    img_path = dd
     result = ocr.ocr(img_path, cls=True)
     for i in range(len(result[0])):
         alt.append(result[0][i][-1][0])
